66e04449a4a138a40defedd335a509889cc460a5
Ricardo Olarte <raop155@hotmail.com>
2018-09-20 23:13:52 -0500

    trying to parse special characters

--- php/mysql/functions.php
+++ php/mysql/functions.php
@@ -1,18 +1,21 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-
+<?php header('Content-Type: text/html; charset=UTF-8');
  $file = "medical-report2.csv";
  $handle = fopen($file, "r");
  $c = 0;
  while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
  {
+  $filesop = array_map("utf8_encode", $filesop); //added
  //$name = $filesop[0];
  //$email = $filesop[1];
 
- echo $filesop[0].'<br/>';
+ $disease = $filesop[0];
+ echo $disease.'<br/>';
 
  //$sql = mysql_query("INSERT INTO xls (name, email) VALUES ('$name','$email')");
  }
+
+ $test = "Nuñez";
+echo $test;
 
  if($sql){
  echo "You database has imported successfully";
@@ -20,3 +23,6 @@ header('Content-Type: text/html; charset=utf-8');
  echo "Sorry! There is some problem.";
  }
  ?>
+</body>
+</html>
+
